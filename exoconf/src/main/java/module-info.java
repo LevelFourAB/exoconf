36627c06b1604325beb30735f9b984e5ca51224f
Andreas Holstenson <a@holstenson.se>
2020-08-15 13:23:46 +0200

    feat(core): Add module-info.java

---
+++ exoconf/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module se.l4.exoconf {
+	requires com.github.spotbugs.annotations;
+
+	requires java.validation;
+	requires org.eclipse.collections.api;
+
+	requires transitive se.l4.exobytes;
+	requires transitive se.l4.ylem.io;
+
+	exports se.l4.exoconf;
+	exports se.l4.exoconf.sources;
+}
